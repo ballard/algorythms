ace4e9f7f4512542427e42cc7e37c47f111c06f3
Ivan <ivan@MacBook-Air-Ivan.local>
2016-12-15 08:07:50 +0300

    search in tree

---
+++ search_node.playground/Contents.swift
@@ -0,0 +1,146 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Node {
+    // Name of the node visible to user
+    var name = ""
+    // If this field is true, children of the Node are shown to user
+    var expanded = false
+    // If this field is true, it means that node matches current search and it is emphasized
+    var matched = false
+    // Children of the node
+    var children: [Node] = []
+    
+    /**
+     * This function returns new subtree of the original tree which satisfies the following requirements:
+     *   - Function doesn't modify original tree
+     *   - The 'Node matches the search' term means that Node's name contains the 'search' as a substring (case insensitive)
+     *   - Node is included in the resulting subtree if Node, one of its ancestors, or one of its descendants matches the search
+     *   - If Node matches the search, its matched property must be set to true, otherwise false
+     *   - If at least one descendant of the Node matches the search, Node's expanded property must be set to true, otherwise false
+     */
+    func search(search: String) -> Node? {
+        
+        let name = self.name
+        var matched = false
+        var expanded = false
+        var children = [Node]()
+        
+        if name.localizedCaseInsensitiveContains(search) {
+            matched = true
+            children = self.children
+        }
+        
+        for selfChild in self.children {
+            if let child = selfChild.search(search: search) {
+                for (index, value) in children.enumerated() {
+                    if value.name == child.name {
+                        children.remove(at: index)
+                    }
+                }
+
+                children.append(child)
+                
+                if children.count != 0 {
+                    expanded = true
+                }
+            }
+        }
+        
+        if matched || expanded {
+            let node = Node()
+            node.name = name
+            node.matched = matched
+            node.expanded = expanded
+            node.children = children
+            return node
+        }
+        
+        return nil
+    }
+}
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        var s = "\(name) (m: \(matched), e: \(expanded))"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
+    }
+}
+
+let root = Node()
+root.name = "root"
+
+let alpha = Node()
+alpha.name = "alpha"
+
+let beta = Node()
+beta.name = "beta"
+
+let gamma = Node()
+gamma.name = "gamma"
+
+let delta = Node()
+delta.name = "delta"
+
+let epsilon = Node()
+epsilon.name = "epsilon"
+
+let dzeta = Node()
+dzeta.name = "dzeta"
+
+let etha = Node()
+etha.name = "etha"
+
+let tetha = Node()
+tetha.name = "tetha"
+
+let yota = Node()
+yota.name = "yota"
+
+let cappa = Node()
+cappa.name = "cappa"
+
+let lambda = Node()
+lambda.name = "lambda"
+
+let mu = Node()
+mu.name = "mu"
+
+let nu = Node()
+nu.name = "nu"
+
+root.children.append(alpha)
+root.children.append(beta)
+
+alpha.children.append(gamma)
+alpha.children.append(delta)
+alpha.children.append(epsilon)
+
+beta.children.append(dzeta)
+
+gamma.children.append(etha)
+
+delta.children.append(tetha)
+
+epsilon.children.append(yota)
+epsilon.children.append(cappa)
+
+dzeta.children.append(lambda)
+dzeta.children.append(mu)
+dzeta.children.append(nu)
+
+print("Initial tree:\n \(root.description)\n\r")
+
+if let searchedTree = root.search(search: "l") {
+    print("Created tree:\n \(searchedTree.description)\n\r")
+}
+
+print("Check initial tree:\n \(root.description)\n\r")
+
+
